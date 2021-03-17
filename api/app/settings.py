3a0922180d6f8fde09510cacf77469a296410120
Vikrant Dubey <vikrant@cuebook.ai>
2021-03-17 18:06:16 +0530

    Adding static data and db folder

--- api/app/settings.py
+++ api/app/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db/db.sqlite3',
     }
 }
 
--- api/start_server.sh
+++ api/start_server.sh
@@ -3,6 +3,8 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (python manage.py createsuperuser --no-input)
 fi
+python manage.py migrate
+python manage.py loaddata seeddata/*.json
 (gunicorn app.wsgi --user www-data --bind 0.0.0.0:8000 --workers 3) &
 (celery -A app worker -B --scheduler django_celery_beat.schedulers:DatabaseScheduler) &
 nginx -g "daemon off;"
