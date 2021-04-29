bc3e33bf6efbedbde098b9a6f4e3450fa5e4ee48
PraveenCuebook <praveen@cuebook.ai>
2021-04-29 16:50:38 +0530

    Update settings.py

--- api/app/settings.py
+++ api/app/settings.py
@@ -85,16 +85,26 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ.get("POSTGRES_DB_SCHEMA"),
-        "USER": os.environ.get("POSTGRES_DB_USERNAME"),
-        "PASSWORD": os.environ.get("POSTGRES_DB_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_DB_HOST"),
-        "PORT": os.environ.get("POSTGRES_DB_PORT"),
+if(os.environ.get("POSTGRES_DB_HOST",False)):
+    """ Postgres is default db for development """
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.environ.get("POSTGRES_DB_SCHEMA"),
+            "USER": os.environ.get("POSTGRES_DB_USERNAME"),
+            "PASSWORD": os.environ.get("POSTGRES_DB_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_DB_HOST"),
+            "PORT": os.environ.get("POSTGRES_DB_PORT"),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db/db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
