7afc8d2413183df25bd802b6f0a92bc6ac013bcc
Vikrant Dubey <vikrant@cuebook.ai>
2021-05-03 18:00:47 +0530

    Adding embed support

--- ui/src/components/Notebooks/NotebookView.js
+++ ui/src/components/Notebooks/NotebookView.js
@@ -2,11 +2,17 @@ import React from "react";
 import style from "./style.module.scss";
 import { useParams } from 'react-router-dom';
 import apiService from "services/api";
+import { useHistory } from "react-router-dom";
 
 export default function NotebookView() {
   const params = useParams();
+  const history = useHistory();
   let notebookId = params.notebookId;
-  let iframeUrl = apiService.zeppelin_base_url + "/#/notebook/" + notebookId;
+  let iframeUrl = "";
+  if(history.location.pathname.indexOf("api/redirect") !== -1)
+    iframeUrl = "/api/redirect/cuelake" + apiService.zeppelin_base_url + "/#/notebook/" + notebookId;
+  else
+    iframeUrl = apiService.zeppelin_base_url + "/#/notebook/" + notebookId;
   return (
     <>
       <iframe className={style.iframe} src={iframeUrl}></iframe>
--- ui/src/components/Settings/ZeppelinInterpreterSettings.js
+++ ui/src/components/Settings/ZeppelinInterpreterSettings.js
@@ -1,9 +1,15 @@
 import React from "react";
 import style from "./style.module.scss";
 import apiService from "services/api";
+import { useHistory } from "react-router-dom";
 
 export default function ZeppelinInterpreterSettings() {
-  let iframeUrl = apiService.zeppelin_base_url + "/#/interpreter";
+  const history = useHistory();
+  let iframeUrl = "";
+  if(history.location.pathname.indexOf("api/redirect") !== -1)
+    iframeUrl = "/api/redirect/cuelake" + apiService.zeppelin_base_url + "/#/interpreter";
+  else
+    iframeUrl = apiService.zeppelin_base_url + "/#/interpreter";
   return (
     <>
       <iframe className={style.iframe} src={iframeUrl}></iframe>
