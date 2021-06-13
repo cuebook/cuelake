bdea2df74487614b686005d9c11e1b95e15a1ff8
Vikrant Dubey <vikrant@cuebook.ai>
2021-06-13 21:54:56 +0530

    Upgrading aiohttp and django

--- api/genie/tests/test_views_schedules.py
+++ api/genie/tests/test_views_schedules.py
@@ -1,17 +1,12 @@
-from django.test import TestCase
 import pytest
-import unittest
-from unittest import mock
-from django.test import TestCase
 from django.urls import reverse
-from mixer.backend.django import mixer
 from conftest import populate_seed_data
 from genie.models import CustomSchedule as Schedule
 
 @pytest.mark.django_db(transaction=True)
 def test_schedules(client, populate_seed_data, mocker):
 
-    # ======================= create schedule test =======================
+    # Create schedule test
     path = reverse('scheduleView')
     data = {'name': 'Schedule at 3 AM ',
              'crontab': '0 3 * * *',
@@ -22,7 +17,7 @@ def test_schedules(client, populate_seed_data, mocker):
     assert response.data['data']
     scheduleId = response.data["data"]
 
-     # ======================= update schedule test ======================
+    # Update schedule test
     path = reverse('scheduleView')
     data = {'id': scheduleId,
              'name': 'Schedule at 4 AM ',
@@ -32,20 +27,20 @@ def test_schedules(client, populate_seed_data, mocker):
     assert response.status_code == 200
     assert Schedule.objects.get(id=scheduleId).name == "Schedule at 4 AM "
 
-    # ======================= get schedule test =========================
+    # Get schedule test
     path = reverse('scheduleView')
     response = client.get(path)
     assert response.status_code == 200
     assert response.data["data"]
 
-    # ======================= getSingleSchedule test =====================
+    # Get single schedule test
     path = reverse("getSingleSchedule", kwargs={"scheduleId": scheduleId})
     response = client.get(path)
     assert response.status_code == 200
     assert response.data["data"]
     assert response.data["data"][0]["name"] == "Schedule at 4 AM "
 
-    # ======================= delete SingleSchedule test =====================
+    # Delete schedule test
     path = reverse("getSingleSchedule", kwargs={"scheduleId": scheduleId})
     response = client.delete(path)
     assert response.status_code == 200
