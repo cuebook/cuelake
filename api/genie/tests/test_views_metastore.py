5c87da450ded81dcfabcc615c1d363a0fe16da8d
Vikrant Dubey <vikrant@cuebook.ai>
2021-07-19 21:40:31 +0530

    #29 Fix for test cases

--- api/genie/tests/test_views_metastore.py
+++ api/genie/tests/test_views_metastore.py
@@ -8,6 +8,7 @@ from conftest import populate_seed_data
 @pytest.mark.django_db
 def test_getMetastoreTables(client, populate_seed_data, mocker):
     path = reverse('metastoreTables')
+    mocker.patch("psycopg2.connect")
     mocker.patch("genie.services.Metastore.executeSQL", return_value = [
         {"id": 1, "table": "testtable", "type": "VIRTUAL_VIEW", "database": "default", "size": None, "last_updated": "1620749748"},
         {"id": 1, "table": "testtable2", "type": "EXTERNAL_TABLE", "database": "default", "size": 233434, "last_updated": "1620749748"}
@@ -20,6 +21,7 @@ def test_getMetastoreTables(client, populate_seed_data, mocker):
 @pytest.mark.django_db
 def test_getMetastoreColumns(client, populate_seed_data, mocker):
     path = reverse('metastoreColumns', kwargs={"tableId": 1})
+    mocker.patch("psycopg2.connect")
     mocker.patch("genie.services.Metastore.executeSQL", return_value = [
         {"name": "Col", "tableId": 1, "type": "decimal"},
         {"name": "Col2", "tableId": 1, "type": "decimal"},
